94ecdd95fef666d1fc3b9179e1465cf39e01324a
carolyne <pcmaunda@gmail.com>
2022-05-30 00:04:21 +0300

    created image class

--- mygallery/myphotos/admin.py
+++ mygallery/myphotos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from myphotos.models import Image
+
+admin.site.register(Image)
--- mygallery/myphotos/models.py
+++ mygallery/myphotos/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils import timezone
+
+
+class Image(models.Model):
+ user = models.OneToOneField(User,on_delete=models.CASCADE)
+ first_name = models.CharField(max_length=15)
+ description = models.TextField()
+ date_joined = models.DateTimeField(auto_now_add=timezone)
