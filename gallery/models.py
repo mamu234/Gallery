faac60d3937b7a1668f6ecb3721009aa3a25c398
carolyne <pcmaunda@gmail.com>
2022-06-01 13:51:43 +0300

    created test file

--- gallery/models.py
+++ gallery/models.py
@@ -1,25 +1,54 @@
-from unicodedata import category
+
 from django.db import models
 
 
 # Create your models here.
 
 
-class Category(models.Model):
+class Location(models.Model):
     name = models.CharField(max_length =50)
 
-def __str__(self):
-    return self.name
+    @classmethod
+    def tag_locations(cls):
+        tags = cls.objects.all()
+        return tags
 
-class tags(models.Model):
-    name = models.CharField(max_length =30)
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
 
     def __str__(self):
         return self.name
 
-class Location(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length =50)
-    post = models.TextField()
-    category = models.ForeignKey(Category)
-    tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.name
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,4 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Category,Location
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category=Category(name = 'food')
+
+def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location=Location(name = 'Afirca')
+
+def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
 
