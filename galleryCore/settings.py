e34e7d94dcec4dc12944fa54c2bf73cefec57e1b
carolyne <pcmaunda@gmail.com>
2022-06-10 19:41:01 +0300

    updated settings

--- galleryCore/settings.py
+++ galleryCore/settings.py
@@ -42,12 +42,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['']
 
 
-cloudinary.config( 
+cloudinary.config(
     cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key = os.getenv('CLOUDINARY_API_KEY'),
     api_secret = os.getenv("CLOUDINARY_API_SECRET"),
-    secure = True
+    secure = True 
 )
+
 
 
 # Application definition
