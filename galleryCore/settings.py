8cd93aa996581da6b539f1aa792f9c8c1c96fedf
carolyne <pcmaunda@gmail.com>
2022-06-10 09:38:43 +0300

    deployed app

--- galleryCore/settings.py
+++ galleryCore/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('django-insecure-v(jp^*myc!)+pc2a2%a^z(woju33@wlj_q*swwc_2s*ftd2+9*')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
