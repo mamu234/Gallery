ae9ff6ed820a3dc40aa837fcf16374be888b102f
carolyne <pcmaunda@gmail.com>
2022-06-10 10:44:29 +0300

    updated setting for static files

--- galleryCore/settings.py
+++ galleryCore/settings.py
@@ -147,12 +147,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
