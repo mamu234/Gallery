1c8bcb13e431a4e93c0670bc8f168d754a0d616e
carolyne <pcmaunda@gmail.com>
2022-06-14 08:31:38 +0300

    installed dotenv

--- galleryCore/settings.py
+++ galleryCore/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = ('django-insecure-v(jp^*myc!)+pc2a2%a^z(woju33@wlj_q*swwc_2s*ftd2+9*')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
