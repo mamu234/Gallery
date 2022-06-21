668838e955790d476d007ce64ec19cd779aefd15
carolyne <pcmaunda@gmail.com>
2022-06-21 16:58:08 +0300

    updated heroku host

--- galleryCore/settings.py
+++ galleryCore/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gallery-apps.herokuapp.com']
 
 
 
